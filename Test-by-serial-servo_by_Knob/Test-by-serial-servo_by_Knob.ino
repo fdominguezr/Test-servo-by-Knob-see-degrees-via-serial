a44444d840f4bbbe5f29fa277f6e3e05647720fc
Francisco Dominguez <fdominguezroman@gmail.com>
2015-04-22 23:05:08 +0200

    Moves a servo by a knob and see degrees at serial
    Arduino:
    reads Knob position via Analog 0
    writes normalized value of position in degrees via serial
    moves a servo via digital 9

---
+++ Test-by-serial-servo_by_Knob/Test-by-serial-servo_by_Knob.ino
@@ -0,0 +1,43 @@
+/* 
+ --- TEST A SERVO WITH A KNOB AND READS POSITION VIA SERIAL FOR ARDUINO ---
+ 
+ Controlling a servo position using a potentiometer (variable resistor) 
+ by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
+
+ modified on 8 Nov 2013
+ by Scott Fitzgerald
+ http://arduino.cc/en/Tutorial/Knob
+
+ Modified on 22nd Apr 2015
+ by Francisco Dominguez
+ See projects at https://github.com/fdominguezr
+ 
+ Now Arduino:
+ reads Knob position via Analog 0
+ writes normalized value of position in degrees via serial
+ moves a servo via digital 9
+
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+void setup()
+{
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+   Serial.begin(9600);
+}
+
+void loop() 
+{ 
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = map(val, 0, 1023, -1, 169);     // scale it to use it with the servo (value between 0 and 180) 
+  Serial.println(val);
+  myservo.write(val);                  // sets the servo position according to the scaled value 
+  delay(15);                           // waits for the servo to get there 
+} 
+
